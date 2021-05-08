56e484408d8ebb98921d54c76b9c23628c48ac96
Narek Khachatryan <Narek_Khachatryan1@epam.com>
2021-05-08 18:02:02 +0400

    Add Chakra and configure it - Update auth logic - Create users table and add auth user in them - Add alias import path config - Add prettierrc.js config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AuthProvider } from '@/libs/auth';
+import customTheme from '@/styles/theme';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <title>Fast Feedback Demo</title>
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
